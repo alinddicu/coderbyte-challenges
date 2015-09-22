c729dbba0a70233828ed638b102630723972959c
alinddicu <alinddicu@yahoo.com>
2015-09-22 20:14:09 +0200

    Implemented a CircularArray

---
+++ CoderbyteChallenges/CoderbyteChallenges/Tools/CircularArray.cs
@@ -0,0 +1,26 @@
+﻿namespace CoderbyteChallenges.Tools
+{
+    using System.Collections.Generic;
+
+    public class CircularArray<T>
+    {
+        private readonly List<T> _list;
+
+        public CircularArray(params T[] array)
+        {
+            _list = new List<T>(array);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _list[index % _list.Count];
+            }
+            set
+            {
+                _list[index] = value;
+            }
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Tools/CircularArrayTest.cs
@@ -0,0 +1,35 @@
+﻿namespace CoderbyteChallenges.Tools
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class CircularArrayTest
+    {
+        [TestMethod]
+        public void WhenGetByIndexWhenExistingThenReturn()
+        {
+            var array = new CircularArray<int>(0, 1);
+
+            Check.That(array[0]).IsEqualTo(0);
+            Check.That(array[1]).IsEqualTo(1);
+        }
+
+        [TestMethod]
+        public void WhenGetByIndexWhenNotExistingThenReturn()
+        {
+            var array = new CircularArray<int>(0, 1);
+
+            Check.That(array[2]).IsEqualTo(0);
+        }
+
+        [TestMethod]
+        public void WhenSetByIndexThenOk()
+        {
+            var array = new CircularArray<int>(0);
+            array[0] = 1;
+
+            Check.That(array[0]).IsEqualTo(1);
+        }
+    }
+}

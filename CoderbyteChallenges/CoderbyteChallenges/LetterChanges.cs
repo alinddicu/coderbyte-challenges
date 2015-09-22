a844bcfa570f4aebda93cbdb5eb928783d31013b
alinddicu <alinddicu@yahoo.com>
2015-09-22 17:24:14 +0200

    LetterChanges 1st step

---
+++ CoderbyteChallenges/CoderbyteChallenges/LetterChanges.cs
@@ -0,0 +1,48 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LetterChanges
+    {
+        public static readonly string[] Letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public string Execute(string initString)
+        {
+            var letters = initString.ToArray().Select(c => c + string.Empty);
+            var returnString = string.Empty;
+            foreach (var letter in letters)
+            {
+                returnString += GetNextLetter(letter);
+            }
+
+            return returnString;
+        }
+
+        private static string GetNextLetter(string letter)
+        {
+            if (letter == "z")
+            {
+                return "a";
+            }
+
+            return Letters[GetIndex(letter) + 1];
+        }
+
+        private static int GetIndex(string letter)
+        {
+            for (var index = 0; index < Letters.Length; index++)
+            {
+                if (letter == Letters[index])
+                {
+                    return index;
+                }
+            }
+
+            throw new InvalidOperationException("Letter not found : " + letter);
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/LetterChangesTest.cs
@@ -0,0 +1,38 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class LetterChangesTest
+    {
+        /*
+         
+         Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
+         Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
+         
+         */
+
+        [TestMethod]
+        [Ignore]
+        public void GivenHello3WhenLetterChangesThenReturnIfmmp3()
+        {
+            var result = new LetterChanges().Execute("Hello3");
+
+            Check.That(result).IsEqualTo("Ifmmp3");
+        }
+
+        [TestMethod]
+        public void GivenheWhenLetterChangesThenReturnif()
+        {
+            var result = new LetterChanges().Execute("he");
+
+            Check.That(result).IsEqualTo("if");
+        }
+    }
+}

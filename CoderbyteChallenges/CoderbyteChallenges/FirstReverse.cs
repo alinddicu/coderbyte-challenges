95f6bce1699b720503b7fa9c0cfc770fd9dcf0b1
alinddicu <alinddicu@yahoo.com>
2015-09-22 15:50:33 +0200

    refactoring

--- CoderbyteChallenges/CoderbyteChallenges/FirstReverse.cs
+++ CoderbyteChallenges/CoderbyteChallenges/FirstReverse.cs
@@ -7,12 +7,22 @@
             var reversedString = string.Empty;
             var charArray = intialString.ToCharArray();
 
-            for (var index = charArray.Length - 1; index >= 0; index--)
+            for (var index = GetLastCaracterPosition(charArray); HasMoreCaractersToReverse(index); index--)
             {
                 reversedString += charArray[index];
             }
 
             return reversedString;
+        }
+
+        private static bool HasMoreCaractersToReverse(int index)
+        {
+            return index >= 0;
+        }
+
+        private static int GetLastCaracterPosition(char[] charArray)
+        {
+            return charArray.Length - 1;
         }
     }
 }

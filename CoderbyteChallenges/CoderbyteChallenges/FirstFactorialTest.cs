7f5ae7729bd10a3a5ab261b99228fccd0fba0f58
alinddicu <alinddicu@yahoo.com>
2015-09-22 16:03:57 +0200

    first factorial

---
+++ CoderbyteChallenges/CoderbyteChallenges/FirstFactorialTest.cs
@@ -0,0 +1,25 @@
+﻿namespace CoderbyteChallenges
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class FirstFactorialTest
+    {
+        [TestMethod]
+        public void Given0WhenFactorialThenReturn1()
+        {
+            var result = new FirstFactorial().Execute(0);
+
+            Check.That(result).IsEqualTo(1);
+        }
+
+        [TestMethod]
+        public void Given3WhenFactorialThenReturn6()
+        {
+            var result = new FirstFactorial().Execute(3);
+
+            Check.That(result).IsEqualTo(6);
+        }
+    }
+}

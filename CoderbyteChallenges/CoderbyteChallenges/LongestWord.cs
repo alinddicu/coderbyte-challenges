c48f88d8cb0f28c68565768e313e920d6a53c5c8
alinddicu <alinddicu@yahoo.com>
2015-09-22 16:16:27 +0200

    LongestWord

---
+++ CoderbyteChallenges/CoderbyteChallenges/LongestWord.cs
@@ -0,0 +1,26 @@
+﻿namespace CoderbyteChallenges
+{
+    public class LongestWord
+    {
+        public string Execute(string phrase)
+        {
+            var words = phrase.Split(' ');
+
+            var longestWord = string.Empty;
+            foreach(var word in words)
+            {
+                if (IsNotLongestWord(longestWord, word))
+                {
+                    longestWord = word;
+                }
+            }
+
+            return longestWord;
+        }
+
+        private static bool IsNotLongestWord(string longestWord, string word)
+        {
+            return word.Length > longestWord.Length;
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/LongestWordTest.cs
@@ -0,0 +1,25 @@
+﻿namespace CoderbyteChallenges
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class LongestWordTest
+    {
+        [TestMethod]
+        public void Given1WordOf2And1WordOf3WhenLongestWordThenReturnTheWordOf3()
+        {
+            var result = new LongestWord().Execute("I am young");
+
+            Check.That(result).IsEqualTo("young");
+        }
+
+        [TestMethod]
+        public void GivenEmptyPhraseWhenLongestWordThenEmpty()
+        {
+            var result = new LongestWord().Execute("");
+
+            Check.That(result).IsEqualTo("");
+        }
+    }
+}

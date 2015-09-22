c688036fc3703ea2731f68b06a5c492c1d148086
alinddicu <alinddicu@yahoo.com>
2015-09-22 14:02:56 +0200

    FirstReverse

---
+++ CoderbyteChallenges/CoderbyteChallenges/FirstReverse.cs
@@ -0,0 +1,18 @@
+﻿namespace CoderbyteChallenges
+{
+    public class FirstReverse
+    {
+        public string Execute(string intialString)
+        {
+            var reversedString = string.Empty;
+            var charArray = intialString.ToCharArray();
+
+            for (var index = charArray.Length - 1; index >= 0; index--)
+            {
+                reversedString += charArray[index];
+            }
+
+            return reversedString;
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/FirstReverseTest.cs
@@ -0,0 +1,18 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class FirstReverseTest
+    {
+        [TestMethod]
+        public void GivenAbCWhenExecuteThenReturnCbA()
+        {
+            var result = new FirstReverse().Execute("AbC");
+
+            Check.That(result).Equals("CbA");
+        }
+    }
+}

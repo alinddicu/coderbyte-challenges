78a8eb074292b976816d08d5f67feba5b1fb590a
alinddicu <alinddicu@yahoo.com>
2015-09-22 16:03:43 +0200

    first factorial

---
+++ CoderbyteChallenges/CoderbyteChallenges/FirstFactorial.cs
@@ -0,0 +1,21 @@
+﻿namespace CoderbyteChallenges
+{
+    public class FirstFactorial
+    {
+        public int Execute(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            var factorial = 1;
+            for (int countDown = n; countDown > 0; countDown--)
+            {
+                factorial = factorial * countDown;
+            }
+
+            return factorial;
+        }
+    }
+}

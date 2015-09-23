12e506a52c39eee9ad5d483c8138307abc7d0a06
alinddicu <alinddicu@yahoo.com>
2015-09-23 11:27:16 +0200

    doing LetterCapitalize

---
+++ CoderbyteChallenges/CoderbyteChallenges/LetterCapitalize.cs
@@ -0,0 +1,16 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LetterCapitalize
+    {
+        public string Execute(string input)
+        {
+            return input.Substring(0, 1).ToUpper() + input.Remove(0, 1);
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/LetterCapitalizeTest.cs
@@ -0,0 +1,37 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class LetterCapitalizeTest
+    {
+        /*
+         
+         Take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space. 
+         
+         */
+
+        [TestMethod]
+        public void GivenHelloWhenExecuteThenReturnHello()
+        {
+            var result = new LetterCapitalize().Execute("hello");
+
+            Check.That(result).IsEqualTo("Hello"); ;
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void GivenhelloworldWhenExecuteThenReturnHelloWorld()
+        {
+            var result = new LetterCapitalize().Execute("hello world");
+
+            Check.That(result).IsEqualTo("Hello World"); ;
+        }
+    }
+}

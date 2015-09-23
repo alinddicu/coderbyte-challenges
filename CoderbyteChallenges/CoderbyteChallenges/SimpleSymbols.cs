eae345fa2f40212b0808a0bd3bd49131ba9f5d90
alinddicu <alinddicu@yahoo.com>
2015-09-23 12:34:27 +0200

    first test SimpleSymbols

---
+++ CoderbyteChallenges/CoderbyteChallenges/SimpleSymbols.cs
@@ -0,0 +1,38 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Tools;
+
+    public class SimpleSymbols
+    {
+        private static readonly string[] LettersLowerCase = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        private static readonly string[] LettersCapitalCase = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        private static readonly string[] Others = { "=", "+" };
+
+        private static readonly string[] AllowedCaracters =
+            LettersLowerCase
+            .Concat(LettersCapitalCase)
+            .Concat(Others)
+            .ToArray();
+
+        public string Execute(string str)
+        {
+            if (HasDisAllowedCaracters(str))
+            {
+                throw new InvalidDataException();
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private static bool HasDisAllowedCaracters(string str)
+        {
+            return !str.ToArrayOfStrings().Any(s => AllowedCaracters.Contains(s));
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/SimpleSymbolsTest.cs
@@ -0,0 +1,46 @@
+﻿namespace CoderbyteChallenges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class SimpleSymbolsTest
+    {
+        /*
+         
+         take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. 
+         The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) 
+            and for the string to be true each letter must be surrounded by a + symbol. 
+         So the string to the left would be false. 
+         The string will not be empty and will have at least one letter. 
+         
+         */
+
+        [TestMethod]
+        [Ignore]
+        public void GivenInput1WhenSimpleSymbolsThenReturnTrue()
+        {
+            Check.That(new SimpleSymbols().Execute("+d+=3=+s+")).IsEqualTo("true");
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void GivenInput2WhenSimpleSymbolsThenReturnFalse()
+        {
+            Check.That(new SimpleSymbols().Execute("f++d+")).IsEqualTo("false");
+        }
+
+        [TestMethod]
+        public void GivenDisallowedCaractersWhenSimpleSymbolsThenThrow()
+        {
+            Check.ThatCode(() => new SimpleSymbols().Execute("0123456789")).Throws<InvalidDataException>();
+            Check.ThatCode(() => new SimpleSymbols().Execute("&#'(-_)~#{[|`\\^@]}$£%µ*,;:!?./§<>")).Throws<InvalidDataException>();
+        }
+    }
+}

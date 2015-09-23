e8f0a12da405b277b0780e2eaf0ed1acc8c0dd07
alinddicu <alinddicu@yahoo.com>
2015-09-23 11:46:07 +0200

    renaming

--- CoderbyteChallenges/CoderbyteChallenges/LetterCapitalize.cs
+++ CoderbyteChallenges/CoderbyteChallenges/LetterCapitalize.cs
@@ -10,7 +10,8 @@
         public string Execute(string input)
         {
             var words = SplitIntoWords(input);
-            return string.Join(Delimiter, words.Select(w => CapitalizeWord(w)));
+            words = Capitalize(words);
+            return JoinCapitalized(words);
         }
 
         private static string[] SplitIntoWords(string input)
@@ -18,9 +19,19 @@
             return input.Split(new[] { Delimiter }, StringSplitOptions.RemoveEmptyEntries);
         }
 
-        private static string CapitalizeWord(string word)
+        private static string[] Capitalize(string[] words)
+        {
+            return words.Select(w => Capitalize(w)).ToArray();
+        }
+
+        private static string Capitalize(string word)
         {
             return word.Substring(0, 1).ToUpper() + word.Remove(0, 1);
+        }
+
+        private static string JoinCapitalized(string[] words)
+        {
+            return string.Join(Delimiter, words);
         }
     }
 }

cb5325acb70eabdf732cc7a415f2e89c8041a802
alinddicu <alinddicu@yahoo.com>
2015-09-23 14:01:15 +0200

    removed usings

--- CoderbyteChallenges/CoderbyteChallenges/SimpleSymbols.cs
+++ CoderbyteChallenges/CoderbyteChallenges/SimpleSymbols.cs
@@ -1,11 +1,8 @@
 ﻿namespace CoderbyteChallenges
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Tools;
 
     public class SimpleSymbols
--- CoderbyteChallenges/CoderbyteChallenges/SimpleSymbolsTest.cs
+++ CoderbyteChallenges/CoderbyteChallenges/SimpleSymbolsTest.cs
@@ -1,11 +1,6 @@
 ﻿namespace CoderbyteChallenges
 {
-    using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using NFluent;
 

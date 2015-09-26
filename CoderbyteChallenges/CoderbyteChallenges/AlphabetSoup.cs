940d65f8cd906c5777bd1d055ae081de619b9f83
alinddicu <alinddicu@yahoo.com>
2015-09-26 11:52:22 +0200

    refactorig AlphabetSoup

--- CoderbyteChallenges/CoderbyteChallenges/AlphabetSoup.cs
+++ CoderbyteChallenges/CoderbyteChallenges/AlphabetSoup.cs
@@ -1,17 +1,16 @@
 ﻿namespace CoderbyteChallenges
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Tools;
 
     public class AlphabetSoup
     {
         public string Execute(string input)
         {
-            return string.Join(string.Empty, input.ToArrayOfStrings().OrderBy(s => s));
+            return input
+                .ToArrayOfStrings()
+                .OrderBy(s => s)
+                .Join();
         }
     }
 }

2dd4a15b970d18f87a273ac70dea90e685a65086
alinddicu <alinddicu@yahoo.com>
2015-09-26 09:50:34 +0200

    abcheck test 1

---
+++ CoderbyteChallenges/CoderbyteChallenges/AbCheck.cs
@@ -0,0 +1,13 @@
+﻿namespace CoderbyteChallenges
+{
+    public class AbCheck
+    {
+        public bool Execute(string str)
+        {
+            var indexOfA = str.IndexOf('a');
+            var expectedIndexOfB = indexOfA + 3;
+
+            return str.IndexOf('b') == expectedIndexOfB;
+        }
+    }
+}

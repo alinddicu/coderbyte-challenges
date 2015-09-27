e3c12f0d23bcca56b01abfdbfd2fc5db67455676
alinddicu <alinddicu@yahoo.com>
2015-09-27 17:29:49 +0200

    ExOh first implementation

---
+++ CoderbyteChallenges/CoderbyteChallenges/ExOh.cs
@@ -0,0 +1,20 @@
+﻿namespace CoderbyteChallenges
+{
+    using System.Linq;
+    using Tools;
+
+    public class ExOh
+    {
+        public bool Execute(string str)
+        {
+            var groups = str
+                .ToArrayOfStrings()
+                .GroupBy(s => s)
+                .Select(g => new {Letter = g.Key, Count = g.ToList().Count() })
+                .ToArray();
+
+            return groups.Count() == 2 
+                && groups.Single(g => g.Letter == "o").Count == groups.Single(g => g.Letter == "x").Count;
+        }
+    }
+}

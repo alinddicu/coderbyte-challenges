bf1e5f2891f28f26f5dcd58982e02e2bcc6ec073
alinddicu <alinddicu@yahoo.com>
2015-09-27 20:42:15 +0200

    did Palindrome

---
+++ CoderbyteChallenges/CoderbyteChallenges/Palindrome.cs
@@ -0,0 +1,23 @@
+﻿namespace CoderbyteChallenges
+{
+    using System.Linq;
+    using Tools;
+
+    public class Palindrome
+    {
+        public bool Execute(string str)
+        {
+            str = str.Replace(" ", string.Empty);
+
+            return str
+                .ToArrayOfStrings()
+                .Select((letter, index) => new { Letter = letter, OppositeLetter = GetOppositeLetter(str, index) })
+                .All(o => o.Letter == o.OppositeLetter);
+        }
+
+        private static string GetOppositeLetter(string str, int index)
+        {
+            return str[str.Length - 1 - index] + string.Empty;
+        }
+    }
+}

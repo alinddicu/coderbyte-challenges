da3a7af0b4a73367f9ac170d93918f52972e97a3
alinddicu <alinddicu@yahoo.com>
2015-10-05 22:50:25 +0200

    first tu on DashInsert

---
+++ CoderbyteChallenges/CoderbyteChallenges/Easy/DashInsert.cs
@@ -0,0 +1,37 @@
+﻿namespace CoderbyteChallenges.Easy
+{
+    using System;
+    using System.Linq;
+    using Tools;
+
+    public class DashInsert
+    {
+        public string Execute(string str)
+        {
+            return str
+                .ToArrayOfStrings()
+                .Select((s, index) => new { S = s, IsOdd = IsOdd(s), Index = index })
+                .Select(
+                    o =>
+                        new
+                        {
+                            o.S,
+                            o.IsOdd,
+                            IsPreviousOdd = o.Index > 0 && IsOdd(str[o.Index]),
+                        })
+                .Select(o => new { S = o.S, Dash = o.IsOdd && o.IsPreviousOdd ? "-" : string.Empty })
+                .Select(o => o.Dash + o.S)
+                .Join();
+        }
+
+        private static bool IsOdd(string s)
+        {
+            return int.Parse(s) % 2 == 1;
+        }
+
+        private static bool IsOdd(char s)
+        {
+            return int.Parse(s + "") % 2 == 1;
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Easy/DashInsertTest.cs
@@ -0,0 +1,24 @@
+﻿namespace CoderbyteChallenges.Easy
+{
+    using System.Security.Cryptography;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class DashInsertTest
+    {
+        /*
+         
+         have the function DashInsert(str) insert dashes ('-') between each two odd numbers in str. 
+         For example: if str is 454793 the output should be 4547-9-3. 
+         Don't count zero as an odd number
+         
+         */
+
+        [TestMethod]
+        public void Given99946WhenDashInsertThen9Dash9Dash946()
+        {
+            Check.That(new DashInsert().Execute("99946")).IsEqualTo("9-9-946");
+        }
+    }
+}

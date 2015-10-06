d12c42514bc4689e0113d8e4c331a34d01c46225
alinddicu <alinddicu@yahoo.com>
2015-10-06 21:48:22 +0200

    did PowersOfTwo

---
+++ CoderbyteChallenges/CoderbyteChallenges/Easy/PowersofTwo.cs
@@ -0,0 +1,21 @@
+﻿namespace CoderbyteChallenges.Easy
+{
+    public class PowersOfTwo
+    {
+        public bool Execute(int number)
+        {
+            do
+            {
+                var rest = number % 2;
+                if (rest != 0)
+                {
+                    return false;
+                }
+
+                number = number / 2;
+            } while (number != 1);
+
+            return true;
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Easy/PowersofTwoTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CoderbyteChallenges.Easy
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class PowersOfTwoTest
+    {
+        [TestMethod]
+        public void Given4WhenPowersofTwoThenTrue()
+        {
+            Check.That(new PowersOfTwo().Execute(4)).IsTrue();
+        }
+
+        [TestMethod]
+        public void Given6WhenPowersofTwoThenTrue()
+        {
+            Check.That(new PowersOfTwo().Execute(6)).IsFalse();
+        }
+    }
+}

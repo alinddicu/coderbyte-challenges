b8df88583f167558e86ca03451d9fb784fe6fa99
alinddicu <alinddicu@yahoo.com>
2015-10-08 21:36:55 +0200

    refactored MultiplicativePersistence

--- CoderbyteChallenges/CoderbyteChallenges/Tools/Extensions.cs
+++ CoderbyteChallenges/CoderbyteChallenges/Tools/Extensions.cs
@@ -8,18 +8,8 @@
         public static int Multiply(this IEnumerable<int> intsEnum)
         {
             var intsList = intsEnum.ToArray();
-            if (!intsList.Any())
-            {
-                return 0;
-            }
 
-            var product = 1;
-            for (var index = 0; index < intsList.Length; index++)
-            {
-                product *= intsList[index];
-            }
-
-            return product;
+            return !intsList.Any() ? 0 : intsList.Aggregate(1, (current, t) => current * t);
         }
     }
 }

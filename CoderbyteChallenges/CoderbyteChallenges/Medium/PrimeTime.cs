33cded3a4c982251653ad866669e0bbc0159cd8c
alinddicu <alinddicu@yahoo.com>
2015-10-12 22:21:46 +0200

    did PrimeTime

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/PrimeTime.cs
@@ -0,0 +1,24 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Linq;
+
+    public class PrimeTime
+    {
+        public bool Execute(int number)
+        {
+            if (number < 3)
+            {
+                return false;
+            }
+
+            var numbers = new int[number - 3];
+            for (var i = 2; i < number - 1; i++)
+            {
+                numbers[i - 2] = i;
+            }
+
+            return numbers.All(n => number % n != 0);
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/PrimeTimeClass.cs
@@ -0,0 +1,43 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class PrimeTimeClass
+    {
+        /*
+         
+         have the function PrimeTime(num) take the num parameter being passed and 
+            return the string true if the parameter is a prime number, 
+            otherwise return the string false. 
+         The range will be between 1 and 2^16.          
+         
+         */
+
+        [TestMethod]
+        public void Given012WhenPrimeTimeThenReturnFalse()
+        {
+            new List<int> { 0, 1, 2 }.ForEach(n => Check.That(new PrimeTime().Execute(n)).IsFalse());
+        }
+
+        [TestMethod]
+        public void Given5WhenPrimeTimeThenReturnTrue()
+        {
+            Check.That(new PrimeTime().Execute(5)).IsTrue();
+        }
+
+        [TestMethod]
+        public void Given19WhenPrimeTimeThenReturnTrue()
+        {
+            Check.That(new PrimeTime().Execute(19)).IsTrue();
+        }
+
+        [TestMethod]
+        public void Given111WhenPrimeTimeThenReturnFalse()
+        {
+            Check.That(new PrimeTime().Execute(111)).IsFalse();
+        }
+    }
+}

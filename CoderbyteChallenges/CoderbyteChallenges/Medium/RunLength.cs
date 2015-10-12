fefffeea86e931cc88360c942db7b0ccf42b1dc4
alinddicu <alinddicu@yahoo.com>
2015-10-12 22:45:43 +0200

    started RunLength

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/RunLength.cs
@@ -0,0 +1,31 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Linq;
+    using Tools;
+
+    public class RunLength
+    {
+        public string Execute(string toCompress)
+        {
+            //return toCompress
+            //    .ToArrayOfStrings()
+            //    .Select((s, i) => new { S = s, Index = i })
+            //    .Select(o => new { o.S, IsSameNext = IsSameNext(toCompress, o.S, o.Index) })
+            //    .GroupBy(o => new {} )
+            //    .Join();
+
+            throw new NotImplementedException();
+        }
+
+        private static bool IsSameNext(string toCompress, string c, int index)
+        {
+            if (index > toCompress.Length - 2)
+            {
+                return false;
+            }
+
+            return c == toCompress.ToArrayOfStrings().ToArray()[index];
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/RunLengthTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class RunLengthTest
+    {
+        /*
+         
+         have the function RunLength(str) take the str parameter being passed and return a compressed version of the string 
+         using the Run-length encoding algorithm. 
+         This algorithm works by taking the occurrence of each repeating character and outputting that number 
+         along with a single character of the repeating sequence. 
+         For example: "wwwggopp" would return 3w2g1o2p. 
+          
+         The string will not contain any numbers, punctuation, or symbols. 
+         
+         */
+
+        [TestMethod]
+        public void GivenaabbcdeWhenRunLengthThenReturn2a2b1c1d1e()
+        {
+            Check.That(new RunLength().Execute("aabbcde")).IsEqualTo("2a2b1c1d1e");
+        }
+    }
+}

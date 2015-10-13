31fef2928d22f9a589eb91976a9903c9a69639a9
alinddicu <alinddicu@yahoo.com>
2015-10-13 14:21:59 +0200

    Refactored RunLength

--- CoderbyteChallenges/CoderbyteChallenges/Medium/RunLength.cs
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/RunLength.cs
@@ -1,7 +1,7 @@
 ﻿namespace CoderbyteChallenges.Medium
 {
-    using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using Tools;
 
@@ -12,60 +12,29 @@
         public string Execute(string toCompress)
         {
             var letters = toCompress.ToArrayOfStrings();
-
             var previousLetter = string.Empty;
-            foreach (var g in letters.Select((l, i) => new {Letter = l, Index = i }))
+            foreach (var letter in letters)
             {
-                var letter = g.Letter;
-                if (!_groups.Any())
-                {
-                    _groups.Add(new Group(letter, g.Index));
-                    previousLetter = letter;
-
-                    continue;
-                }
-
-                var group = _groups.Last();
                 if (letter == previousLetter)
                 {
-                    group.Count++;
+                    _groups.Last().Count++;
                 }
                 else
                 {
-                    _groups.Add(new Group(letter, g.Index));
+                    _groups.Add(new Group(letter));
                 }
 
                 previousLetter = letter;
             }
 
             return _groups.Select(g => g.ToString()).Join();
-
-            //return toCompress
-            //    .ToArrayOfStrings()
-            //    .Select((s, i) => new { S = s, Index = i })
-            //    .Select(o => new { o.S, IsSameNext = IsSameNext(toCompress, o.S, o.Index) })
-            //    .GroupBy(o => new {} )
-            //    .Join();
-
-            throw new NotImplementedException();
-        }
-
-        private static bool IsSameNext(string toCompress, string c, int index)
-        {
-            if (index > toCompress.Length - 2)
-            {
-                return false;
-            }
-
-            return c == toCompress.ToArrayOfStrings().ToArray()[index];
         }
 
         private class Group
         {
-            public Group(string letter, int firstLetterGlobalPosition)
+            public Group(string letter)
             {
                 Letter = letter;
-                FirstLetterGlobalPosition = firstLetterGlobalPosition;
                 Count = 1;
             }
 
@@ -73,11 +42,9 @@
 
             public int Count { get; set; }
 
-            public int FirstLetterGlobalPosition { get; private set; }
-
             public override string ToString()
             {
-                return string.Format("{0}{1}", Count, Letter);
+                return string.Format(CultureInfo.InvariantCulture, "{0}{1}", Count, Letter);
             }
         }
     }

21dd0f19eed59dcbe3ec45c2731b9cf40ecc1664
alinddicu <alinddicu@yahoo.com>
2015-10-14 17:46:17 +0200

    did StringScramble

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/StringScramble.cs
@@ -0,0 +1,43 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System.Linq;
+    using Tools;
+
+    public class StringScramble
+    {
+        public bool Execute(string random, string word)
+        {
+            var wordLetterInfos = WordToLetterInfos(word);
+            var randomLetterInfos = WordToLetterInfos(random);
+
+            return wordLetterInfos.All(w => randomLetterInfos.SingleOrDefault(r => w.Letter == r.Letter).Count >= w.Count);
+        }
+
+        private static System.Collections.Generic.IEnumerable<LetterInfo> WordToLetterInfos(string word)
+        {
+            return word
+                    .ToArrayOfStrings()
+                    .GroupBy(l => l)
+                    .Select(g => new LetterInfo(g.Key, g.Count()));
+        }
+
+        private struct LetterInfo
+        {
+            public LetterInfo(string letter, int count)
+                : this()
+            {
+                Letter = letter;
+                Count = count;
+            }
+
+            public string Letter { get; private set; }
+
+            public int Count { get; private set; }
+
+            public override string ToString()
+            {
+                return string.Format("Letter : {0}, Count : {1}", Letter, Count);
+            }
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/StringScrambleTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class StringScrambleTest
+    {
+        /*
+         
+         have the function StringScramble(str1,str2) take both parameters being passed and return the string true 
+         if a portion of str1 characters can be rearranged to match str2, 
+         otherwise return the string false. 
+         
+         For example: if str1 is "rkqodlw" and str2 is "world" the output should return true. Punctuation and symbols will not be entered with the parameters. 
+         
+         */
+
+        [TestMethod]
+        public void GivencdoreAndcoderWhenStringScrambleThenReturnTrue()
+        {
+            Check.That(new StringScramble().Execute("cdore","coder")).IsTrue();
+        }
+
+        [TestMethod]
+        public void Givenh3llkoAndhelloWhenStringScrambleThenReturnTrue()
+        {
+            Check.That(new StringScramble().Execute("h3llko", "hello")).IsFalse();
+        }
+    }
+}

3b7d1fdbd479e708d486e518a9cb7de982f68a8d
alinddicu <alinddicu@yahoo.com>
2015-10-14 21:26:41 +0200

    did BinaryConverter

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/BinaryConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using Tools;
+
+    public class BinaryConverter
+    {
+        public int Execute(int binaryNumber)
+        {
+            return binaryNumber
+                .ToString(CultureInfo.InvariantCulture)
+                .ToArrayOfStrings()
+                .Reverse()
+                .Select((s, i) => new { S = s, Index = i })
+                .Select(o => int.Parse(o.S) * (int)Math.Pow(2, o.Index))
+                .Sum(i => i);
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/BinaryConverterTest.cs
@@ -0,0 +1,28 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class BinaryConverterTest
+    {
+        /*
+         
+            have the function BinaryConverter(str) return the decimal form of the binary value. 
+            For example: if 101 is passed return 5, or if 1000 is passed return 8. 
+         
+         */
+
+        [TestMethod]
+        public void Given101WhenBinaryConverterThenReturn5()
+        {
+            Check.That(new BinaryConverter().Execute(101)).IsEqualTo(5);
+        }
+
+        [TestMethod]
+        public void Given1000WhenBinaryConverterThenReturn8()
+        {
+            Check.That(new BinaryConverter().Execute(1000)).IsEqualTo(8);
+        }
+    }
+}

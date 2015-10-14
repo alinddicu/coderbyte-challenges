c3c9213d31031e2c8e9459f88bdb7f289436f34e
alinddicu <alinddicu@yahoo.com>
2015-10-14 14:50:21 +0200

    started GreatestCommonFactor

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/GreatestCommonFactor.cs
@@ -0,0 +1,22 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CoderbyteChallenges.Tools;
+
+    public class GreatestCommonFactor
+    {
+        public int Execute(int n1, int n2)
+        {
+            var primeNumbers = new PrimeNumbers().Execute(1000).ToArray();
+
+            var factors1 = primeNumbers.Where(pn => pn < n1).Where(pn => n1 % pn == 0).Concat(new List<int> { 1 });
+            var factors2 = primeNumbers.Where(pn => pn < n2).Where(pn => n2 % pn == 0).Concat(new List<int> { 1 });
+
+            return factors1.Intersect(factors2).Multiply();
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/GreatestCommonFactorTest.cs
@@ -0,0 +1,38 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class GreatestCommonFactorTest
+    {
+        /*
+         
+         have the function Division(num1,num2) take both parameters being passed and return the Greatest Common Factor. 
+         That is, return the greatest number that evenly goes into both numbers with no remainder. 
+          
+         For example: 12 and 16 both are divisible by 1, 2, and 4 so the output should be 4. 
+         The range for both parameters will be from 1 to 10^3. 
+         
+         
+         */
+
+        [TestMethod]
+        public void Given7And3WhenDivisionThenReturn1()
+        {
+            Check.That(new GreatestCommonFactor().Execute(7,3)).IsEqualTo(1);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void Given36And54WhenDivisionThenReturn18()
+        {
+            Check.That(new GreatestCommonFactor().Execute(36, 54)).IsEqualTo(18);
+        }
+    }
+}

73207e84af9ee49d5eafc4c534fd47e960435253
alinddicu <alinddicu@yahoo.com>
2015-10-16 12:39:01 +0200

    did TripleDouble

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/TripleDouble.cs
@@ -0,0 +1,40 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Tools;
+
+    public class TripleDouble
+    {
+        public bool Execute(int tripleInt, int doubleInt)
+        {
+            var tripleDigits = ToDigitGroup(tripleInt, 3);
+            var doubleDigits = ToDigitGroup(doubleInt, 2);
+
+            return tripleDigits.Any(t => doubleDigits.Select(d => d.Digit).Contains(t.Digit));
+        }
+
+        private static IEnumerable<DigitGroup> ToDigitGroup(int tripleInt, int groupCount)
+        {
+            return tripleInt
+                .ToString()
+                .ToArrayOfStrings()
+                .GroupBy(o => o)
+                .Select(g => new DigitGroup(g.Key, g.Count()))                
+                .Where(d => d.Count >= groupCount);
+        }
+
+        private struct DigitGroup
+        {
+            public DigitGroup(string digit, int count) : this()
+            {
+                Digit = digit;
+                Count = count;
+            }
+
+            public string Digit { get; private set; }
+
+            public int Count { get; private set; }
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/TripleDoubleTest.cs
@@ -0,0 +1,32 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class TripleDoubleTest
+    {
+        /*
+         
+            have the function TripleDouble(num1,num2) take both parameters being passed, 
+            and return true if there is a straight triple of a digit at any place in num1 and also a straight double of the same digit in num2. 
+         
+            For example: if num1 equals 451999277 and num2 equals 41177722899, then return 1 because 
+            in the first parameter you have the straight triple 999 and you have a straight double, 99, of the same number in the second parameter. 
+            If this isn't the case, return false. 
+         
+        */
+
+        [TestMethod]
+        public void Given465555And5579WhenExecuteThenReturnTrue()
+        {
+            Check.That(new TripleDouble().Execute(465555, 5579)).IsTrue();
+        }
+
+        [TestMethod]
+        public void Given67844And66237WhenExecuteThenReturnTrue()
+        {
+            Check.That(new TripleDouble().Execute(67844, 66237)).IsFalse();
+        }
+    }
+}

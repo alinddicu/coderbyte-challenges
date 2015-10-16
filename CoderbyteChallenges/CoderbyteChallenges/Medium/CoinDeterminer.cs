5cd4718cacc9d07bf83181e5474a403fd9d81b36
alinddicu <alinddicu@yahoo.com>
2015-10-16 23:50:59 +0200

    started CoinDeterminer

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/CoinDeterminer.cs
@@ -0,0 +1,23 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System.Linq;
+
+    public class CoinDeterminer
+    {
+        private static readonly int[] Coins = { 1, 5, 7, 9, 11 };
+
+        public int Execute(int sum)
+        {
+            var count = 0;
+            var rest = sum;
+            do
+            {
+                var coin = Coins.LastOrDefault(c => rest - c >= 0);
+                rest = rest - coin;
+                count++;
+            } while (rest > 0);
+
+            return count;
+        }
+    }
+}

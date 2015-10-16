22efc680734a8094d43f9914e9e53f32bebf2726
alinddicu <alinddicu@yahoo.com>
2015-10-16 23:11:24 +0200

    did ThreeFiveMultiples

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/ThreeFiveMultiples.cs
@@ -0,0 +1,15 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System.Linq;
+
+    public class ThreeFiveMultiples
+    {
+        public int Execute(int num)
+        {
+            return Enumerable
+                .Range(1, num - 1)
+                .Where(n => n % 3 == 0 || n % 5 == 0)
+                .Sum(n => n);
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/ThreeFiveMultiplesTest.cs
@@ -0,0 +1,23 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class ThreeFiveMultiplesTest
+    {
+        /*
+         
+         have the function ThreeFiveMultiples(num) return the sum of all the multiples of 3 and 5 that are below num. 
+         For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, and 9, 
+         and adding them up you get 23, so your program should return 23.
+         
+         */
+
+        [TestMethod]
+        public void Given10WhenThreeFiveMultiplesThenReturn23()
+        {
+            Check.That(new ThreeFiveMultiples().Execute(10)).IsEqualTo(23);
+        }
+    }
+}

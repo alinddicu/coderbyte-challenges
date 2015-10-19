d1751c5fd9032c976d1b79fc26b606c5f645cfbd
alinddicu <alinddicu@yahoo.com>
2015-10-19 22:34:33 +0200

    some solution on CoinDeterminer

--- CoderbyteChallenges/CoderbyteChallenges/Medium/CoinDeterminer.cs
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/CoinDeterminer.cs
@@ -1,18 +1,21 @@
 ﻿namespace CoderbyteChallenges.Medium
 {
+    using System;
     using System.Linq;
 
     public class CoinDeterminer
     {
         private static readonly int[] Coins = { 1, 5, 7, 9, 11 };
+        private static readonly int MaxCoin = Coins.Max(c => c);
 
         public int Execute(int sum)
         {
-            var count = 0;
-            var rest = sum;
+            var minNumberOfCoins = Math.Round((decimal)sum / MaxCoin, MidpointRounding.AwayFromZero);
+            var count = 1;
+            var rest = sum - MaxCoin;
             do
             {
-                var coin = Coins.LastOrDefault(c => rest - c >= 0);
+                var coin = Coins.Except(new[] { MaxCoin }).LastOrDefault(c => rest - c >= 0);
                 rest = rest - coin;
                 count++;
             } while (rest > 0);

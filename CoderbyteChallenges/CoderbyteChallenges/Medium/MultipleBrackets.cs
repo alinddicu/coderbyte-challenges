4914e911bc40a031bc4112604139f2f94f926706
alinddicu <alinddicu@yahoo.com>
2015-10-20 15:25:49 +0200

    started MultipleBrackets

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/MultipleBrackets.cs
@@ -0,0 +1,35 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Tools;
+
+    public class MultipleBrackets
+    {
+        private static readonly IEnumerable<string> LeftBrackets = new[] { "(", "[" };
+        private static readonly IEnumerable<string> RightBrackets = new[] { ")", "]" };
+        private static readonly IEnumerable<string> Brackets = LeftBrackets.Union(RightBrackets);
+
+        public MultipleBracketsResult Execute(string phrase)
+        {
+            var brackets = phrase.ToArrayOfStrings().Where(c => Brackets.Contains(c));
+            var leftBrackets = brackets.Where(c => LeftBrackets.Contains(c));
+            var rightBrackets = brackets.Where(c => RightBrackets.Contains(c));
+
+            return new MultipleBracketsResult(leftBrackets.Count() == rightBrackets.Count());
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct MultipleBracketsResult
+    {
+        private bool _bracketsMatch;
+
+        public MultipleBracketsResult(bool bracketsMatch)
+        {
+            _bracketsMatch = bracketsMatch;
+        }
+    }
+}

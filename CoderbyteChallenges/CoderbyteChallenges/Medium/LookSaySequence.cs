50f9c76d98f6aa764cb24de79a09dd894db46f43
alinddicu <alinddicu@yahoo.com>
2015-10-21 22:27:16 +0200

    started LookSaySequence

---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/LookSaySequence.cs
@@ -0,0 +1,48 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Tools;
+
+    public class LookSaySequence
+    {
+        public int Execute(int num)
+        {
+            var strings = num
+                .ToString(CultureInfo.InvariantCulture)
+                .ToArrayOfStrings();
+
+            var groups = new List<List<string>>();
+            var lastString = strings.First();
+            var group = new List<string>();
+            for (var i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (lastString == s)
+                {
+                    group.Add(s);
+                    if (i == strings.Length - 1)
+                    {
+                        groups.Add(group);
+                    }
+                }
+                else
+                {
+                    groups.Add(group);
+                    group = new List<string> { s };
+                }
+
+                lastString = s;
+            }
+
+            return int.Parse(groups
+                .Select(g => new { Digit = g.First(), Count = g.Count() })
+                .Select(o => o.Count.ToString(CultureInfo.InvariantCulture) + o.Digit)
+                .Join());
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CoderbyteChallenges/CoderbyteChallenges/Medium/LookSaySequenceTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CoderbyteChallenges.Medium
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class LookSaySequenceTest
+    {
+        /*
+         
+            have the function LookSaySequence(num) take the num parameter being passed and return the next number 
+            in the sequence according to the following rule:
+         
+            to generate the next number in a sequence read off the digits of the given number, 
+            counting the number of digits in groups of the same digit. 
+         
+            For example, the sequence beginning with 1 would be: 1, 11, 21, 1211, ... 
+            The 11 comes from there being "one 1" before it and the 21 comes from there being 
+            "two 1's" before it. 
+        
+            So your program should return the next number in the sequence given num.         
+         
+         */
+
+        [TestMethod]
+        public void Given1211WhenLookSaySequenceThenReturn111221()
+        {
+            Check.That(new LookSaySequence().Execute(1211)).IsEqualTo(111221);
+        }
+    }
+}

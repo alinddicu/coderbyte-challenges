d4aa5579850872b7a7c44a72674319d403943a01
alinddicu <alinddicu@yahoo.com>
2015-10-25 17:48:59 +0100

    started

--- CoderbyteChallenges/CoderbyteChallenges/Hard/TransitivityRelations.cs
+++ CoderbyteChallenges/CoderbyteChallenges/Hard/TransitivityRelations.cs
@@ -1,6 +1,52 @@
 ﻿namespace CoderbyteChallenges.Hard
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Tools;
+
     public class TransitivityRelations
     {
+        private static readonly string[] RemovableChars = { "(", ")", "," };
+
+        public TransitivityRelationsResult Execute(params string[] stringMatrixes)
+        {
+            var matrix = new Matrix(stringMatrixes);
+
+            throw new System.NotImplementedException();
+        }
+
+        private class Matrix
+        {
+            private readonly int[][] _members;
+
+            public Matrix(params string[] stringMatrixes)
+            {
+                _members = Parse(stringMatrixes);
+            }
+
+            private static int[][] Parse(params string[] stringMatrixes)
+            {
+                var returnLines = new List<int[]>();
+                foreach (var line in stringMatrixes)
+                {
+                    returnLines.Add(
+                        line.ToArrayOfStrings()
+                            .Where(s => !RemovableChars.Contains(s))
+                            .Select(int.Parse).ToArray());
+                }
+
+                return returnLines.ToArray();
+            }
+        }
+
+        public class TransitivityRelationsResult
+        {
+            public TransitivityRelationsResult(bool isTransitive)
+            {
+                IsTransitive = isTransitive;
+            }
+
+            public bool IsTransitive { get; private set; }
+        }
     }
 }
--- CoderbyteChallenges/CoderbyteChallenges/Hard/TransitivityRelationsTest.cs
+++ CoderbyteChallenges/CoderbyteChallenges/Hard/TransitivityRelationsTest.cs
@@ -1,9 +1,16 @@
 ﻿namespace CoderbyteChallenges.Hard
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
 
     [TestClass]
     public class TransitivityRelationsTest
     {
+        [TestMethod]
+        public void GivenTest1WhenExecuteThenReturnTrue()
+        {
+            var result = new TransitivityRelations().Execute("(1,1,1)","(0,1,1)","(0,1,1)"); 
+            Check.That(result.IsTransitive).IsTrue();
+        }
     }
 }
